0ec0f6b1acaa78643aaaae6dad5b4603c3d61b53
mvonpapen <m.von.papen@fz-juelich.de>
2017-09-15 14:34:31 +0200

    sts_exp now loaded in compute function

--- tests.py
+++ tests.py
@@ -5,24 +5,35 @@ Created on Fri Sep 15 11:34:28 2017
 @author: papen
 """
 
+## Imports
 from scipy.stats import levene
 import sciunit
 import numpy as np
 import analyze_data as ana
 import load_data
 from quantities import ms
+import load_data as ld
+import analyze_data as ad
+
 
 # ======================TEST SCORE AND TEST===============================
-
 class LeveneScore(sciunit.Score):
     """
     A Levene Test score.
     Null Ho: homogeneity of variance or homoscedasticity
     
     """    
+    
+    
     @classmethod
     def compute(self, model):
-        pvalue = levene(model.covar, exp.covar).pvalue
+        #first load experimental data and store spiketrains
+        sts_exp = ld.load_nikos2rs(path2file  = './', 
+                      class_file = './simrest_validation/nikos2rs_consistency_EIw035complexc04.txt',
+                      eiThres    = 0.4)
+        Cexp    = ad.cross_covariance(sts_exp, binsize = 150*ms)
+        # Now pipe model and exp into actual test
+        pvalue = levene(model.covar.ravel(), Cexp.ravel()).pvalue
         self.score = LeveneScore(pvalue)
         return self.score
     
@@ -32,6 +43,7 @@ class LeveneScore(sciunit.Score):
                   + "If the resulting p-value of Levene's test is less than some significance level "
                   + "(typically 0.05), the obtained differences in sample variances are unlikely to have "
                   + "occurred based on random sampling from a population with equal variances.")
+    
     
     @property
     def sort_key(self):
@@ -49,13 +61,15 @@ class CovarianceTest(sciunit.Test, LeveneScore):
     required_capabilities = (ProducesSpikeTrains,)
     score_type = LeveneScore # This test's 'judge' method will return a Levenecore.
     
+    
     def generate_prediction(self, model, verbose=False):
         """
         Implementation of sciunit.Test.general_prediction.
         Generates a prediction from a model using the required capabilities.
         """
-        model.produce_covar()
+        model.cross_covariance()
         return model 
+        
     
     def validate_observation(self, observation, first_try=True):
         """
@@ -80,34 +94,7 @@ class CovarianceTest(sciunit.Test, LeveneScore):
         prediction returned in the previous step.
         This comparison of quantities is cast into a score.
         '''
-        
-        # get covars from experiment
-        # set path
-        datadir    = './'
-        class_file = './simrest_validation/nikos2rs_consistency_EIw035complexc04.txt'
-        
-        # set parameters
-        binsize  = 150*pq.ms
-        nbins    = 100
-        binrange = [-0.6, 0.6]
-        eiThres  = 0.4
-        
-        # load data
-        sts_mod = get_spiketrains(model)
-        sts_exp = load_data.load_nikos2rs(path2file = datadir,
-                                         eiThres    = eiThres,
-                                         class_file = class_file)
-        
-        # calculate pdf of covariances
-        pdf, bins, Cexp = ana.covariance_analysis(sts_exp, 
-                                                  binsize  = binsize, 
-                                                  binrange = binrange,
-                                                  nbins    = nbins)
-        pdf, bins, Cmod = ana.covariance_analysis(sts_mod, 
-                                                  binsize  = binsize, 
-                                                  binrange = binrange,
-                                                  nbins    = nbins)
-        l = LeveneScore.compute(Cmod.ravel(), Cexp.ravel()) 
+        l = LeveneScore.compute(model) 
         score = LeveneScore(l.score)
         score.description = "There is {} significant difference between the variances of inter spike intervals (p-value={})." \
                             .format('no' if score.score >= p_value else 'a', p_value)
