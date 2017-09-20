e22eb49c8b906c8e8b40553e4ba84a8b4532f8fe
mvonpapen <m.von.papen@fz-juelich.de>
2017-09-20 15:31:28 +0200

    added Robins Levene score

---
+++ networkunit/scores/score_LeveneScore.py
@@ -0,0 +1,49 @@
+import sciunit
+import numpy as np
+from scipy.stats import levene
+
+# COMMENTS ##############################
+#
+# all open issues marked with "## TODO ##"
+#
+#
+# COMMENTS ##############################
+
+
+#==============================================================================
+
+class LeveneScore(sciunit.Score):
+    """
+    A Levene Test score.
+    Null hypothesis: homogeneity of variance or homoscedasticity
+    """
+
+#    _allowed_types = (bool,) ## TODO ## don't know what to set here
+    
+    _description = ("Levene's test is an inferential statistic used to assess the equality of variances. "
+                  + "It tests the null hypothesis that the population variances are equal "
+                  + "(called homogeneity of variance or homoscedasticity). "
+                  + "If the resulting p-value of Levene's test is less than some significance level "
+                  + "(typically 0.05), the obtained differences in sample variances are unlikely to have "
+                  + "occurred based on random sampling from a population with equal variances.")
+
+    @classmethod
+    def compute(cls, observation, prediction):
+        """
+        Computes p-value of probability that variances are equal.
+        """
+        assert isinstance(prediction,dict)
+        assert isinstance(observation,dict)
+        
+        pvalue = dict()
+        for key in prediction.keys():
+            pvalue[key] = levene(prediction[key], observation[key]).pvalue
+        max_pval = np.nanmax(pvalue.values())
+        return LeveneScore(max_pval)
+
+    @property
+    def sort_key(self):
+        return self.score
+
+    def __str__(self):
+        return 'pvalue = {:.3}'.format(self.score)
