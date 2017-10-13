84550f0e045c128dc4cf5b6b0a8fe627a20a099d
mvonpapen <m.von.papen@fz-juelich.de>
2017-10-13 20:28:39 +0200

    return self.spiketrains

--- networkunit/models/model_cortical_microcircuit_data.py
+++ networkunit/models/model_cortical_microcircuit_data.py
@@ -114,6 +114,6 @@ class microcircuit_data_annotate_sts(cortical_microcircuit_data):
             st.annotations['neu_type'] = 'inh'
         for st in sts_exc:
             st.annotations['neu_type'] = 'exc'
-        spiketrains = sts_inh
-        spiketrains.extend(sts_exc)
-        return spiketrains
+        self.spiketrains = sts_inh
+        self.spiketrains.extend(sts_exc)
+        return self.spiketrains
